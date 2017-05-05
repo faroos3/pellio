4b2b997f4003422418078be940f32c50970c35f4
calerc1 <carloscalero14@gmail.com>
2017-05-04 23:08:04 -0400

    added regular expression such that / after a url didnt have to be typed in order to get a valid page request

--- server/pellio/base/urls.py
+++ server/pellio/base/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^registration/', views.registration, name='registration'),
-    url(r'^about/', views.about, name='about'),
-    url(r'^', views.index, name='index'),
-    #url(r'^home/', I
+    url(r'^registration[/]*', views.registration, name='registration'),
+    url(r'^about[/]*', views.about, name='about'),
+    url(r'^home[/]*', views.home, name='home'), 
+    url(r'^[/]*', views.index, name='index'),
 ]
